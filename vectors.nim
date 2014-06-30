d66db96feffe262ab6a8f7eafaaa7ef3117df775
BrendanBenshoof <brendan@glidr.net>
2014-06-30 11:31:12 -0400

    pushed readme

--- vectors.nim
+++ vectors.nim
@@ -9,6 +9,7 @@ type
 
 proc initVector*(dim : int, coords : seq[float]): Vector =
   system.doAssert(dim==coords.len)
+  system.doAssert(dim>=2)
   result.dim = dim
   result.deltas = coords
 
@@ -25,6 +26,13 @@ proc init2dVector(coords : seq[float]): Vector =
 proc init3dVector(coords : seq[float]): Vector =
   result = initVector(3,coords)
 
+
+proc `[]`*(A:Vector, i:int):float=
+  result = A.deltas[i]
+
+iterator items*(A:Vector): float=
+  for i in A.deltas:
+    yield i
 
 proc `+`*(A,B : Vector) : Vector =
   system.doAssert(A.dim == B.dim)
@@ -55,13 +63,20 @@ proc norm*(A : Vector) : float =
 proc unit*(A : Vector) : Vector =
   result = A * norm(A)
 
-proc angle(A,B : Vector) : float =
+proc angle*(A,B : Vector) : float =
   result = arccos((A*B)/(norm(A)*norm(B)))
   #returns inter-Vector angle in radians
 
-#todo
-#cross product for 3d
-# optional : genralized cross product
-# unit-vector from angles
+proc cross*(A,B : Vector) : Vector = 
+  system.doAssert(A.dim == 3 AND B.dim == 3)
+  result = initZeroVector(3)
+  result.deltas[0] = A.deltas[1]*B.deltas[2] - A.deltas[2]*B.deltas[1]
+  result.deltas[1] = A.deltas[2]*B.deltas[0] - A.deltas[0]*B.deltas[2]
+  result.deltas[2] = A.deltas[0]*B.deltas[1] - A.deltas[1]*B.deltas[0]
 
+proc `$`*(A,Vector):string =
+  result = $A.digits
 
+var test: Vector = initZeroVector(10)
+for i in test:
+  echo(i)
